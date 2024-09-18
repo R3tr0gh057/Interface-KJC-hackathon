1115f2619cc19f580533dfb3256b812716bb0ca2
StinkyKronos <hemuhemu85672@gmail.com>
2024-09-18 10:59:10 +0530

    First Commit.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,24 @@
 export default function Home() {
-    return
+	const checkBreach = () => {
+        const email = document.getElementById('email').value;
+        const responseBox = document.getElementById('response');
+
+        fetch(`https://api.xposedornot.com/v1/check-email/${email}`)
+        .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+            console.log(data.breaches)
+            responseBox.innerText = data.breaches
+        })
+    };
+
+	return (
+		<>
+			<input type="email" name="" id="email" />
+			<button onClick={checkBreach}>Check</button>
+
+			<div id="response"></div>
+		</>
+	);
 }
